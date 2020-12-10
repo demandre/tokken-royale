6da7cc7040696b3cc13a313d91f5c25d3789418b
Joris Demandre <joris.demandre@agence-soon.fr>
2020-12-11 00:39:12 +0100

    add election browser in home page

--- vapp/src/App.vue
+++ vapp/src/App.vue
@@ -1,56 +1,129 @@
 <template>
-  <div v-if="isDrizzleInitialized" id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-
-    <div class="section">
-      <h2>Show the Accounts</h2>
-      <drizzle-account units="Ether" :precision="2" />
-    </div>
-
-    <div class="section">
-      <h2>Tutorial Token</h2>
-      <TutorialToken />
-    </div>
-
-    <div class="section">
-      <h2>Simple Storage</h2>
-      <SimpleStorage />
-    </div>
-
-    <div class="section">
-      <h2>Complex Storage</h2>
-      <ComplexStorage />
-    </div>
+  <div id="app" v-if="isDrizzleInitialized">
+    <ElectionBrowser v-bind:elections="elections" />
   </div>
 
-  <div v-else>Loading...</div>
+  <div v-else class="ui active dimmer">
+    <div class="ui text loader">Loading</div>
+  </div>
 </template>
 
 <script>
-import TutorialToken from './TutorialToken'
-import SimpleStorage from './SimpleStorage'
-import ComplexStorage from './ComplexStorage'
-import { mapGetters } from 'vuex'
+  import ElectionBrowser from './ElectionBrowser'
+  import { mapGetters } from 'vuex'
 
-export default {
-  name: 'app',
-  components: {
-    ComplexStorage,
-    TutorialToken,
-    SimpleStorage
-  },
+  export default {
+    name: 'app',
 
-  computed: mapGetters('drizzle', ['isDrizzleInitialized'])
-}
+    components: {
+      ElectionBrowser
+    },
+
+    computed: {
+      ...mapGetters('drizzle', ['isDrizzleInitialized']),
+      elections: function() {
+        return {
+          0: {
+            title: "Bob l'éponge et ses amis 1",
+            previewPicture: "https://upload.wikimedia.org/wikipedia/fr/b/b8/Bob_l%27%C3%A9ponge_-_Logo.png",
+            participants: {
+              0: {
+                name: "Bob l'éponge",
+                avatar: "https://i.pinimg.com/originals/bb/96/a8/bb96a8ccfc7807cdba75cc90260580a1.png"
+              },
+              1: {
+                name: "Plankton",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/1/1f/Bob_l%27%C3%A9ponge_Plankton.png/revision/latest/scale-to-width-down/340?cb=20170310013607&path-prefix=fr"
+              },
+              2: {
+                name: "Patrick",
+                avatar: "https://img.discogs.com/XGSvq-099hah7y1SVfhVx3CnI20=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-5944638-1502980138-6120.jpeg.jpg"
+              },
+              3: {
+                name: "M. Krabs",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/7/7b/Krabs_artwork.png/revision/latest?cb=20190725100918&path-prefix=fr"
+              }
+            }
+          },
+          1: {
+            title: "Bob l'éponge et ses amis 2",
+            previewPicture: "https://upload.wikimedia.org/wikipedia/fr/b/b8/Bob_l%27%C3%A9ponge_-_Logo.png",
+            participants: {
+              0: {
+                name: "Bob l'éponge",
+                avatar: "https://i.pinimg.com/originals/bb/96/a8/bb96a8ccfc7807cdba75cc90260580a1.png"
+              },
+              1: {
+                name: "Plankton",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/1/1f/Bob_l%27%C3%A9ponge_Plankton.png/revision/latest/scale-to-width-down/340?cb=20170310013607&path-prefix=fr"
+              },
+              2: {
+                name: "Patrick",
+                avatar: "https://img.discogs.com/XGSvq-099hah7y1SVfhVx3CnI20=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-5944638-1502980138-6120.jpeg.jpg"
+              },
+              3: {
+                name: "M. Krabs",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/7/7b/Krabs_artwork.png/revision/latest?cb=20190725100918&path-prefix=fr"
+              }
+            }
+          },
+          2: {
+            title: "Bob l'éponge et ses amis 3",
+            previewPicture: "https://upload.wikimedia.org/wikipedia/fr/b/b8/Bob_l%27%C3%A9ponge_-_Logo.png",
+            participants: {
+              0: {
+                name: "Bob l'éponge",
+                avatar: "https://i.pinimg.com/originals/bb/96/a8/bb96a8ccfc7807cdba75cc90260580a1.png"
+              },
+              1: {
+                name: "Plankton",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/1/1f/Bob_l%27%C3%A9ponge_Plankton.png/revision/latest/scale-to-width-down/340?cb=20170310013607&path-prefix=fr"
+              },
+              2: {
+                name: "Patrick",
+                avatar: "https://img.discogs.com/XGSvq-099hah7y1SVfhVx3CnI20=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-5944638-1502980138-6120.jpeg.jpg"
+              },
+              3: {
+                name: "M. Krabs",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/7/7b/Krabs_artwork.png/revision/latest?cb=20190725100918&path-prefix=fr"
+              }
+            }
+          },
+          3: {
+            title: "Bob l'éponge et ses amis 4",
+            previewPicture: "https://upload.wikimedia.org/wikipedia/fr/b/b8/Bob_l%27%C3%A9ponge_-_Logo.png",
+            participants: {
+              0: {
+                name: "Bob l'éponge",
+                avatar: "https://i.pinimg.com/originals/bb/96/a8/bb96a8ccfc7807cdba75cc90260580a1.png"
+              },
+              1: {
+                name: "Plankton",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/1/1f/Bob_l%27%C3%A9ponge_Plankton.png/revision/latest/scale-to-width-down/340?cb=20170310013607&path-prefix=fr"
+              },
+              2: {
+                name: "Patrick",
+                avatar: "https://img.discogs.com/XGSvq-099hah7y1SVfhVx3CnI20=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-5944638-1502980138-6120.jpeg.jpg"
+              },
+              3: {
+                name: "M. Krabs",
+                avatar: "https://vignette.wikia.nocookie.net/bob-leponge/images/7/7b/Krabs_artwork.png/revision/latest?cb=20190725100918&path-prefix=fr"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ vapp/src/ElectionBrowser.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="isDrizzleInitialized">
+       <div class="ui grid doubling stackable center container">
+           <div class="five wide column" v-for="election in this.elections">
+               <a class="ui card shadow" href="">
+                   <img class="ui image" v-bind:src="election.previewPicture" v-bind:alt="election.title">
+                   <div class="content">
+                       <span class="ui label">{{ election.title }}</span>
+                   </div>
+               </a>
+           </div>
+       </div>
+    </div>
+
+    <div v-else>Loading...</div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+
+    export default {
+        name: 'ElectionBrowser',
+        props: {
+            elections: Object
+        },
+
+        computed: {
+            ...mapGetters('drizzle', ['isDrizzleInitialized']),
+        }
+    }
+</script>
+
+<style></style>
